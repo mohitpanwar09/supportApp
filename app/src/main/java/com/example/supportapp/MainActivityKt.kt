0d3c4f9fb629a6df6fe228c0de5b348599ddac9e
mohitpanwar09 <sandypanwar09@gmail.com>
2020-10-24 02:05:38 +0530

    uiChange

---
+++ app/src/main/java/com/example/supportapp/MainActivityKt.kt
@@ -0,0 +1,11 @@
+package com.example.supportapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityKt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_kt)
+    }
+}
--- app/src/main/java/com/example/supportapp/registerActivity/RegisterActivity.kt
+++ app/src/main/java/com/example/supportapp/registerActivity/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.example.supportapp.registerActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import com.example.supportapp.MainActivityKt
 import com.example.supportapp.R
 import com.example.supportapp.ktClass.AddUser
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -30,6 +36,7 @@ class RegisterActivity : AppCompatActivity() {
             getUserStored()
         }
     }
+    
 
     private fun getUserStored(){
         val userName=register_userName.text.toString()
@@ -39,6 +46,9 @@ class RegisterActivity : AppCompatActivity() {
         FirebaseDatabase.getInstance().getReference("User/$userUid").setValue(userDetails)
                 .addOnCompleteListener {
                     Toast.makeText(this,"Successfully Registered",Toast.LENGTH_SHORT).show()
+                    val intent=Intent(this,MainActivityKt::class.java)
+                    intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
                 }
                 .addOnFailureListener{
                     Toast.makeText(this,"Failed to Registered",Toast.LENGTH_SHORT).show()
